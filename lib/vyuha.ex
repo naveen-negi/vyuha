d2d6e762ed46c7bed2667af60450bd6006468e72
Naveen Negi <nav301186@gmail.com>
2017-01-07 12:21:24 +0530

    Adding Vnode and intilializing riak_core

--- lib/vyuha.ex
+++ lib/vyuha.ex
@@ -3,11 +3,13 @@ defmodule Vyuha do
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
+     :ok = :riak_core.register([{:vnode_module, Vyuha.Vnode}])
 
     children = [
+      worker(:riak_core_vnode_master, [Vyuha.Vnode], id: Vuha.Vnode_master_worker)
     ]
 
-    opts = [strategy: :one_for_one, name: Vyuha.Supervisor]
+    opts = [strategy: :one_for_one, name: Vyuha.Supervisor, max_restarts: 5, max_seconds: 10]
     Supervisor.start_link(children, opts)
   end
 end
